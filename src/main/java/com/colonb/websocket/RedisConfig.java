ea8a4e4375061bcae2cc90dfbfe86977f1038e2f
saha3211 <sae_hwi@colon-b.com>
2023-10-18 15:26:39 +0900

    채팅 stomp 적용

--- src/main/java/com/colonb/websocket/RedisConfig.java
+++ src/main/java/com/colonb/websocket/RedisConfig.java
@@ -1,28 +1,15 @@
 package com.colonb.websocket;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import redis.embedded.RedisServer;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.io.IOException;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedisConfig {
-    private RedisServer redisServer;
+   @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+       return new LettuceConnectionFactory("localhost", 6379);
+   }
 
-    public RedisConfig(@Value("${spring.redis.port}") int port) throws IOException {
-        this.redisServer = RedisServer.builder().port(port).setting("maxmemory 128M").build();
-    }
-
-    @PostConstruct
-    public void startRedis() {
-        this.redisServer.start();
-    }
-
-    @PreDestroy
-    public void stopRedis() {
-        this.redisServer.stop();
-    }
 }
